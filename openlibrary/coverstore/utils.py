fe923e4da4fe98c4a07745bd02bb50471e6a9e6c
cclauss <cclauss@me.com>
2020-01-15 14:09:13 +0100

    Revert openlibrary/coverstore/utils.py

--- openlibrary/coverstore/utils.py
+++ openlibrary/coverstore/utils.py
@@ -1,5 +1,7 @@
 """Utilities for coverstore"""
 
+import urllib
+import urllib2
 import socket
 import os
 import mimetypes
@@ -7,7 +9,6 @@ import simplejson
 import web
 
 import random
-from six.moves import urllib
 import string
 
 import config
@@ -19,7 +20,7 @@ except NameError:  # Python 3
     from io import IOBase as file
 
 
-class AppURLopener(urllib.request.FancyURLopener):
+class AppURLopener(urllib.FancyURLopener):
     version = "Mozilla/5.0 (Compatible; coverstore downloader http://covers.openlibrary.org)"
 
 socket.setdefaulttimeout(10.0)
@@ -53,7 +54,7 @@ def ol_things(key, value):
         }
         try:
             d = dict(query=simplejson.dumps(query))
-            result = download(get_ol_url() + '/api/things?' + urllib.parse.urlencode(d))
+            result = download(get_ol_url() + '/api/things?' + urllib.urlencode(d))
             result = simplejson.loads(result)
             return result['result']
         except IOError:
@@ -73,8 +74,8 @@ def ol_get(olkey):
 
 USER_AGENT = "Mozilla/5.0 (Compatible; coverstore downloader http://covers.openlibrary.org)"
 def download(url):
-    req = urllib.request.Request(url, headers={'User-Agent': USER_AGENT})
-    r = urllib.request.urlopen(req)
+    req = urllib2.Request(url, headers={'User-Agent': USER_AGENT})
+    r = urllib2.urlopen(req)
     return r.read()
 
 def urldecode(url):
@@ -87,7 +88,7 @@ def urldecode(url):
     base, query = urllib.splitquery(url)
     query = query or ""
     items = [item.split('=', 1) for item in query.split('&') if '=' in item]
-    d = dict((urllib.parse.unquote(k), urllib.unquote_plus(v)) for (k, v) in items)
+    d = dict((urllib.unquote(k), urllib.unquote_plus(v)) for (k, v) in items)
     return base, d
 
 def changequery(url, **kw):
@@ -97,7 +98,7 @@ def changequery(url, **kw):
     """
     base, params = urldecode(url)
     params.update(kw)
-    return base + '?' + urllib.parse.urlencode(params)
+    return base + '?' + urllib.urlencode(params)
 
 def read_file(path, offset, size, chunk=50*1024):
     """Returns an iterator over file data at specified offset and size.
@@ -140,7 +141,7 @@ def urlencode(data):
             break
 
     if not multipart:
-        return 'application/x-www-form-urlencoded', urllib.parse.urlencode(data)
+        return 'application/x-www-form-urlencoded', urllib.urlencode(data)
     else:
         # adopted from http://code.activestate.com/recipes/146306/
         def get_content_type(filename):
